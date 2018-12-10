4ff25526d2e00d408c1cdb1dc46136210a87421b
Obsiye <mrabdirizak@gmail.com>
2018-12-10 16:15:32 +0000

    Put abbreviate test into note_spec.js and method into note.js

--- spec/abbreviate_spec.js
+++
@@ -1,8 +0,0 @@
-const abbreviate = require('../src/abbreviate.js');
-
-function test_abbreviate() {
-  var short_string = abbreviate("This is a very long string which is more than 20 characters");
-  console.log(short_string === "This is a very long ");
-}
-
-test_abbreviate();
--- spec/note_spec.js
+++ spec/note_spec.js
@@ -8,4 +8,11 @@ function testCreate(){
     console.log(note.notes[0] === 'test note');
 }
 
+function testAbbreviate() {
+    let note1 = new noteClass();
+    var short_string = note1.abbreviate("This is a very long string which is more than 20 characters");
+    console.log(short_string === "This is a very long ");
+}
+
+testAbbreviate();
 testCreate();
