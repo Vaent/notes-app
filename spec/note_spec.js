96916a0aabff80a264a960224dcee41f08d22c92
Al <>
2018-12-10 19:21:03 +0000

    Refactor for styling and clarity
    Combines 'check' and 'isTrue' calls into the tests for comparison with
    functions like 'scenario'/'it' and 'expect' in other frameworks. Also
    improves consistency of spacing and similar.

--- spec/note_spec.js
+++ spec/note_spec.js
@@ -1,24 +1,15 @@
 const noteClass = require('../src/note.js');
-const { check, isTrue} = require('./index.js')
+const { check, isTrue } = require('./index.js');
 
-	function testCreate(){
-    let note = new noteClass();
-
-    note.create('test note');
-
-    return note.notes[0] === 'test note';
-	}
-
-	function testAbbreviate() {
-    let note1 = new noteClass();
-    var short_string = note1.abbreviate("This is a very long string which is more than 20 characters");
-    return short_string === "This is a very long ";
-	}
-
-check('creates new note', () => {
-    isTrue(testCreate());
+check('creates new note', function(){
+  let note = new noteClass();
+  note.create('test note');
+  isTrue(note.notes[0] === 'test note');
 });
 
-check('returns abbreviated string', () => {
-    isTrue(testAbbreviate());
+check('returns abbreviated string', function(){
+  let note = new noteClass();
+	long_string = "This is a very long string which is more than 20 characters";
+  var short_string = note.abbreviate(long_string);
+  isTrue(short_string === "This is a very long ");
 });
