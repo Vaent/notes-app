eff8753028a87a856c71ace1322775939b0ea1d2
Al <>
2018-12-11 16:43:29 +0000

    Display full text of a note by itself

--- src/interface.js
+++ src/interface.js
@@ -13,22 +13,27 @@ let display_notes = function() {
 	let displayed = note.all();
 	let html_text = document.getElementById('note-list');
   html_text.innerHTML = "";
-  
+
   for (let i = 0; i < displayed.length; i++){
 		html_text.innerHTML += "<a href='#" + i + "'>" + note.abbreviate(displayed[i]) + "</a><br/>";
   };
 }
 
-let showNote = function(){
-  getNoteUrl(window.location);
+let showNoteByItself = function(){
+  note_div = document.getElementById('single-note');
+  note_text = note.all()[getNoteIndex(window.location)];
+  note_div.innerHTML = note_text;
+  note_div.removeAttribute('hidden');
+  main_div = document.getElementById('main-screen');
+  main_div.hidden='hidden';
 }
 
-let getNoteUrl = function (location) {
-  console.log(location.hash.split('#')[1]);
+let getNoteIndex = function (location) {
+  return location.hash.split('#')[1];
 }
 
 function listenToUrl(){
-  window.addEventListener("hashchange", showNote);
+  window.addEventListener("hashchange", showNoteByItself);
 };
 
 listenToUrl();
