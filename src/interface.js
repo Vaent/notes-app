5fef8f182f0729449334e7e54cad4b20c4f5a961
Aga <a.dufrat@gmail.com>
2018-12-11 17:06:17 +0000

    Add return button on note's individual page

--- src/interface.js
+++ src/interface.js
@@ -5,11 +5,11 @@ let create = function() {
   let text = document.getElementById('text').value;
 
   note.create(text);
-  display_notes();
+  displayNotes();
   document.getElementById('text').value = "";
 }
 
-let display_notes = function() {
+let displayNotes = function() {
 	let displayed = note.all();
 	let html_text = document.getElementById('note-list');
   html_text.innerHTML = "";
@@ -20,16 +20,24 @@ let display_notes = function() {
 }
 
 let showNoteByItself = function(){
-  note_div = document.getElementById('single-note');
-  note_text = note.all()[getNoteIndex(window.location)];
-  note_div.innerHTML = note_text;
-  note_div.removeAttribute('hidden');
-  main_div = document.getElementById('main-screen');
-  main_div.hidden='hidden';
+  let single_note = document.getElementById('single-note');
+  let note_text_div = document.getElementById('note-text');
+  let note_text = note.all()[getNoteIndex(window.location)];
+  note_text_div.innerHTML = note_text;
+  single_note.removeAttribute('hidden');
+  let main_div = document.getElementById('main-screen');
+  main_div.hidden = 'hidden';
 }
 
 let getNoteIndex = function (location) {
   return location.hash.split('#')[1];
+}
+
+let goBack = function(){
+  let note_div = document.getElementById('single-note');
+  note_div.hidden = 'hidden';
+  let main_div = document.getElementById('main-screen');
+  main_div.removeAttribute('hidden');
 }
 
 function listenToUrl(){
